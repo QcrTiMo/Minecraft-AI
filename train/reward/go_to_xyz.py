02d8807f3f8c5fdedcbbe31b4a8f66c892519ec2
QcrTiMo <qcrtimo@gmail.com>
2025-09-29 20:21:38 +0800

    更新奖励函数

--- train/reward/go_to_xyz.py
+++ train/reward/go_to_xyz.py
@@ -10,31 +10,19 @@ TRUNCATED_PENALTY = reward_config['truncated_penalty']
 ALIVE_PENALTY = reward_config['alive_penalty']
 W_DISTANCE = reward_config['w_distance']
 W_ANGLE = reward_config['w_angle']
-PROXIMITY_THRESHOLD = reward_config['PROXIMITY_THRESHOLD']
-W_PROXIMITY_PENALTY = reward_config['W_PROXIMITY_PENALTY']
 
 def calculate_reward(info: dict, previous_info: dict, terminated: bool, truncated: bool) -> float:
     """
-    计算“移动到指定坐标”任务的奖励。
+    计算“移动到指定坐标”任务的奖励（修改版）。
     """
-    #如果是因成功而终止
     if terminated:
         return REACH_TARGET_REWARD
-    #如果是因超时而截断
     if truncated:
         return TRUNCATED_PENALTY
-    
     distance_reward = previous_info['distance_to_target'] - info['distance_to_target']
     angle_diff = info['angle_diff_to_target']
     angle_reward = math.cos(angle_diff)
-
-    proximity_penalty = 0.0
-    if info['distance_to_target'] < PROXIMITY_THRESHOLD:
-        #惩罚值与离目标的距离成反比：越近，惩罚越大
-        #这会迫使AI尽快完成最后一步，而不是在附近徘徊
-        proximity_penalty = - (1.0 / max(info['distance_to_target'], 0.1)) * W_PROXIMITY_PENALTY
-
-    reward = (distance_reward * W_DISTANCE) + (angle_reward * W_ANGLE) + ALIVE_PENALTY + proximity_penalty
+    reward = (distance_reward * W_DISTANCE) + (angle_reward * W_ANGLE) + ALIVE_PENALTY
     return reward
 
 def is_terminated(info: dict) -> bool:
